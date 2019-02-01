73c63933d1274240df471007f73d591f0e4f3feb
Matthias Hannig <mail@matthiashannig.de>
2019-02-01 20:41:17 +0100

    updated model

--- server/customers/models.py
+++ server/customers/models.py
@@ -9,9 +9,9 @@ class Customer(models.Base):
     __tablename__ = "customer"
 
     # Fields
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String)
+    id = sa.Column(sa.Integer, sa.Sequence('customer_id_seq'), primary_key=True)
+    name = sa.Column(sa.String, nullable=False)
 
     def __repr__(self):
-        return "Customer(id={}, name={})".format(id, name)
+        return "Customer(id={}, name={})".format(self.id, self.name)
 
