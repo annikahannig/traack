f384673095484e058dd62be3e22e83980b417762
Matthias Hannig <mail@matthiashannig.de>
2019-02-01 12:57:18 +0100

    added minimal client

---
+++ client/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	tracker_pb "github.com/mhannig/traak/service/lib/v1/tracker"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("traak v1")
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("localhost:2344", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Create client
+	customers_svc := tracker_pb.NewCustomerServiceClient(conn)
+
+	// Make request
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := customers_svc.CreateCustomer(ctx, &tracker_pb.CreateCustomerRequest{
+		Customer: &tracker_pb.Customer{
+			Name: "Fnordbert.",
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Result:")
+	fmt.Println(res)
+}
