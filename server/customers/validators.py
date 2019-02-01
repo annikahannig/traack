8cd430fd3e23e8d1c952103595c38eeaa9aa4ce3
Matthias Hannig <mail@matthiashannig.de>
2019-02-01 13:00:38 +0100

    added module skeletons

---
+++ server/customers/validators.py
@@ -0,0 +1,12 @@
+
+class CustomerValidator():
+
+    def validate(self, customer):
+        """
+        Validate a customer
+
+        :returns: a tuple of Success and Errors
+        :rtype: tuple(boolean, list)
+        """
+
+        return True, []
